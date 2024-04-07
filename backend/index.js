e8d049924420d74f9f85b25002a25d123038270d
Sachet Subedi <sachetsubedi001@gmail.com>
2024-04-07 21:01:49 +0545

    migrated to render

--- backend/index.js
+++ backend/index.js
@@ -1,70 +1,99 @@
+import cors from "cors";
 import express from "express";
 import { Server } from "socket.io";
-import generateRandomRoomName from './scripts/randomRoomGenerator.js';
-import cors from "cors";
+import generateRandomRoomName from "./src/scripts/randomRoomGenerator.js";
+
 const app = express();
+
+// Apply CORS middleware with specific options
 app.use(cors());
 
 app.get("/", (req, res) => {
-    res.send("Resonse");
+  res.send("Response");
 });
-const httpServer=app.listen(3000, console.log("Server running at 3000"));
-const io= new Server(httpServer,{
-    cors:{
-        origin:'*'
-    }
-})
 
-io.on('connection',(socket)=>{
-    
-    const defaultNames = ['Anonymous Monkey', 'Captain Quirk', 'Luna Stardust', 'Sunny Sideup', 'Pixel Pajamas', 'Whispering Willow', 'Captain Noodle', 'Mystic Muffin', 'Jazzberry Jam', 'Dizzy Dinosaur', 'Cheesequake Cactus', 'Moonlight Marmalade', 'Sir Snugglekins', 'Chuckleberry Finn', 'Bubbles McFluff', 'Doodlebug Daffodil', 'Spaghetti Supernova', 'Giggles McGee', 'Scribble Scribble', 'Twinkle Toes'];
-    let userName;
-    let roomId='master';
-    socket.join(roomId);
+const httpServer = app.listen(3000, () => {
+  console.log("Server running at 3000");
+});
 
-    socket.on('joinRoom',(currentRoom,room)=>{
-        socket.broadcast.to(currentRoom).emit('leftCustomRoom',userName);
-        socket.leave(currentRoom);
-        socket.join(room);
-        socket.emit('roomJoined',room);
-        socket.broadcast.to(room).emit('join',userName);
-        roomId=room;
-    })
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*", // Update this to your front-end domain
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+    credentials: true,
+  },
+});
 
-    socket.on('createRoom',(currentRoom)=>{
-        const room=generateRandomRoomName();
-        socket.broadcast.to(currentRoom).emit('leftCustomRoom',userName);
-        socket.leave(currentRoom);
-        socket.join(room);
-        socket.emit('roomCreated',room);
-        socket.emit('roomJoined',room);
-        roomId=room;
-    })
+io.on("connection", (socket) => {
+  const defaultNames = [
+    "Anonymous Monkey",
+    "Captain Quirk",
+    "Luna Stardust",
+    "Sunny Sideup",
+    "Pixel Pajamas",
+    "Whispering Willow",
+    "Captain Noodle",
+    "Mystic Muffin",
+    "Jazzberry Jam",
+    "Dizzy Dinosaur",
+    "Cheesequake Cactus",
+    "Moonlight Marmalade",
+    "Sir Snugglekins",
+    "Chuckleberry Finn",
+    "Bubbles McFluff",
+    "Doodlebug Daffodil",
+    "Spaghetti Supernova",
+    "Giggles McGee",
+    "Scribble Scribble",
+    "Twinkle Toes",
+  ];
 
-      
-    // send connection message
-    socket.on('createConnection',(name)=>{
-        name!=null?userName=name:userName=defaultNames[Math.floor(Math.random()*20)];
-        socket.broadcast.to(roomId).emit('connected',userName);
-        socket.emit('connectionSuccess',userName);
-    })
-    // message sent
-    socket.on('message',(mess)=>{
-        console.log(mess);
-        io.to(roomId).emit('message',{userName:userName,message:mess});
-    })
+  let userName;
+  let roomId = "master";
+  socket.join(roomId);
 
-    
-    // rename user
-    socket.on('rename',(newUserName)=>{
-        const oldUsername=userName;
-        userName=newUserName;
-        socket.broadcast.to(roomId).emit('rename',{oldUsername,userName});
-        socket.emit('renamed',{oldUsername,userName});
-    })
+  socket.on("joinRoom", (currentRoom, room) => {
+    socket.broadcast.to(currentRoom).emit("leftCustomRoom", userName);
+    socket.leave(currentRoom);
+    socket.join(room);
+    socket.emit("roomJoined", room);
+    socket.broadcast.to(room).emit("join", userName);
+    roomId = room;
+  });
 
-    socket.on('disconnect',(reason)=>{
-        socket.broadcast.to(roomId).emit('disconnected',userName);
-    })
-    
-})
+  socket.on("createRoom", (currentRoom) => {
+    const room = generateRandomRoomName();
+    socket.broadcast.to(currentRoom).emit("leftCustomRoom", userName);
+    socket.leave(currentRoom);
+    socket.join(room);
+    socket.emit("roomCreated", room);
+    socket.emit("roomJoined", room);
+    roomId = room;
+  });
+
+  socket.on("createConnection", (name) => {
+    userName =
+      name != null
+        ? name
+        : defaultNames[Math.floor(Math.random() * defaultNames.length)];
+    socket.broadcast.to(roomId).emit("connected", userName);
+    socket.emit("connectionSuccess", userName);
+  });
+
+  socket.on("message", (mess) => {
+    console.log(mess);
+    io.to(roomId).emit("message", { userName, message: mess });
+  });
+
+  socket.on("rename", (newUserName) => {
+    const oldUsername = userName;
+    userName = newUserName;
+    socket.broadcast.to(roomId).emit("rename", { oldUsername, userName });
+    socket.emit("renamed", { oldUsername, userName });
+  });
+
+  socket.on("disconnect", () => {
+    socket.broadcast.to(roomId).emit("disconnected", userName);
+  });
+});
